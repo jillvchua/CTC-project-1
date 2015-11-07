9fdd6ab70f05b4335ed3f7482ca67239498c8164
jillvchua <jillvchua@gmail.com>
2015-11-07 16:21:43 -0500

    Initial Commit

---
+++ client/router.js
@@ -0,0 +1,19 @@
+Router.route('/', {
+    template: 'about',
+    title: 'cretivitythroughcode-about'
+});
+
+Router.route('/photo-gallery', {
+    template: 'gallery',
+    title: 'creativitythroughcode-Photo Gallery'    
+});
+
+Router.route('/pusheen-styles', {
+    template: 'styles',
+    title: 'creativitythroughcode-Pusheen Styles'
+}); 
+
+Router.route('/contact', {
+    template: 'Contacts',
+    title: 'creativitythroughcode-Contacts'
+}); 
---
+++ server/router.js
@@ -0,0 +1,9 @@
+Router.route('/', {
+    template: 'about',
+    title: 'cretivitythroughcode-about'
+});
+
+Router.route('/Photo gallery', {
+    template: 'Photo Gallery',
+    title: 'creativitythroughcode-Photo Gallery'    
+});
